4be7cb5aefabfd3e4cd1fa99a94f9b7ce1ef9f32
striker-zn <sniperzn2006@gmail.com>
2026-02-20 10:16:38 +0000

    start editing

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
+Route::get('/hello', function () {
+    return ["message" => "fuck niggers"];
+});
