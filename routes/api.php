f2ad8d33485cd0806948a351b2f78dee24a92236
vcx-0047 <mezouarwalid.0047@gmail.com>
2026-03-10 16:10:24 +0100

    testing pusher

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\MessageController;
+use App\Events\MessageNotification;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-
-
-Route::apiResource('/messages', MessageController::class);
+Route::get('/event' , function(){
+    event(new MessageNotification('this is our first broadcast messaage'));
+});
+// Route::get('/listen' , function(){
+//     event(new MessageNotification('this is our first broadcast messaage'));
+// });
+// Route::apiResource('/messages', MessageController::class);
 
 // Route::middleware('auth:sanctum')->group(function () {
 //     Route::apiResource('messages', MessageController::class);
